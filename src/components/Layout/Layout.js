22faddda7c49e8def5568a533fbf303e0a9b7cc4
Borko <petrovicborko392@gmail.com>
2021-02-15 20:30:56 +0100

    Added Starter Files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+const layout = (props) => (
+  <Auxiliary>
+    <div className="layout">Toolbar, Sidedrawer, Backdrop</div>
+    <main>{props.children}</main>
+  </Auxiliary>
+);
+
+export default layout;
